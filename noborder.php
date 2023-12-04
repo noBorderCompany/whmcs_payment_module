a80c123c92a40ce487e82c6e9e045b1dbd0cfa99
noBorder.tech <152797841+noBorderTech@users.noreply.github.com>
2023-12-04 22:29:11 +0330

    Add files via upload

---
+++ noborder.php
@@ -0,0 +1,195 @@
+<?php
+
+/*
+* noBorder payment gateway
+* @developer Hanif Zekri
+* @publisher noBorder
+* @copyright (C) 2020 noBorder
+* @version  1.1
+* @license http://www.gnu.org/licenses/gpl-2.0.html GPLv2 or later
+* https://noborder.tech
+*/
+
+use WHMCS\Database\Capsule;
+
+$invoice_id = intval($_GET['invoiceid']);
+
+if ($invoice_id > 0) {
+
+    require_once __DIR__ . '/../../init.php';
+    require_once __DIR__ . '/../../includes/gatewayfunctions.php';
+    require_once __DIR__ . '/../../includes/invoicefunctions.php';
+
+    $gatewayParams = getGatewayVariables('noborder');
+    if (!$gatewayParams['type']) die('The module is not activated.');
+	
+	$user_id = $_SESSION['uid'];
+	$api_key = $gatewayParams['api_key'];
+	$pay_currency = $gatewayParams['pay_currency'];
+	
+	$action = $_GET['action'];
+	$currency = $_GET['currency'];
+
+	if ($action == 'pay' and $invoice_id > 0 and $user_id > 0) {
+
+        $invoice = Capsule::table('tblinvoices')->where('id', $invoice_id)->where('status', 'Unpaid')->where('userid', $user_id)->first();
+        
+		if (!$invoice)
+			die("Unfortunately, this invoice does not exist or does not belong to you. If you think there is a problem, contact support.");
+
+        $user = Capsule::table('tblclients')->where('id', $user_id)->first();
+        
+        $amount = $invoice->total;
+        /* Remove Added Slash In Version 7 Or Above */
+        $systemurl = rtrim($gatewayParams['systemurl'], '/') . '/';
+
+        $params = array(
+			'api_key' => $api_key,
+			'amount_value' => $amount,
+			'amount_currency' => strtolower($currency),
+			'pay_currency' => $pay_currency,
+            'order_id' => $invoice_id,
+            'respond_type' => 'link',
+            'callback' => $systemurl . 'modules/gateways/noborder.php?action=confirm&invoiceid=' . $invoice_id
+		);
+						
+		$url = 'https://noborder.tech/action/ws/request_create';
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
+		curl_close($ch);
+				
+		$result = json_decode($response);
+		
+		if ($result->status != 'success')
+			echo '<p>The payment gateway encountered an error. <br> Gateway respond :' . $result->respond .'</p>';
+		else {
+			$is_Updated = Capsule::table('tblinvoices')->where('id', $invoice_id)->update(['notes' => $result->request_id]);
+            if ($is_Updated == 1) header('Location: ' . $result->respond);
+            if ($is_Updated == 0) die('The database encountered an error. Please try again and if the problem is not resolved, contact support.');
+        }
+    }
+	
+    if ($action == 'confirm' and $invoice_id > 0 and $user_id > 0){
+		
+		$invoice = Capsule::table('tblinvoices')->where('id', $invoice_id)->where('status', 'Unpaid')->first();
+        
+		if (!$invoice)
+			die("Unfortunately, this invoice does not exist or does not belong to you. If you think there is a problem, contact support.");
+			
+        $checkGateway = checkCbInvoiceID($invoice_id, $gatewayParams['name']);
+        if (!$checkGateway) die("Another payment method has been chosen to pay this invoice.");
+		
+		$params = array(
+			'api_key' => $api_key,
+			'order_id' => $invoice_id,
+			'request_id' => $invoice->notes
+		);
+				
+		$url = 'https://noborder.tech/action/ws/request_status';
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
+		curl_close($ch);
+		
+		$result = json_decode($response);
+		
+		if ($result->status != 'success') {
+			
+			logTransaction($gatewayParams['name'], ["GET" => $_GET, "POST" => $_POST, "result" => $result], 'Failure');
+			
+			$message = noborder_get_filled_message($gatewayParams['failed_massage'], $invoice->notes, $invoice_id);
+            Capsule::table('tblinvoices')->where('id', $invoice_id)->update(['notes' => $message . '<br> Gateway respond : ' . $result->respond]);
+			
+            header('Location: ' . $gatewayParams['systemurl'] . '/viewinvoice.php?id=' . $invoice_id);
+		
+		} else {
+		
+			$verify_status = empty($result->status) ? NULL : $result->status;
+			$verify_request_id = empty($result->request_id) ? NULL : $result->request_id;
+			$verify_amount = empty($result->amount_value) ? NULL : $result->amount_value;
+			
+			$amount = $invoice->total;
+			
+			if (number_format($verify_amount, 5) != number_format($amount, 5)) {
+				$message = noborder_get_filled_message($gatewayParams['failed_massage'], $verify_request_id, $invoice_id);
+				$message .= '<br> Unfortunately, an error occurred in the confirmation process. Please try again or contact support if needed.';
+				
+				logTransaction($gatewayParams['name'], ["GET" => $_GET, "POST" => $_POST, "result" => $result], 'Failure');
+				Capsule::table('tblinvoices')->where('id', $invoice_id)->update(['notes' => $message]);
+				header('Location: ' . $gatewayParams['systemurl'] . '/viewinvoice.php?id=' . $invoice_id);
+			
+			} else {
+				addInvoicePayment($invoice_id, $verify_request_id, $amount, 0, $gatewayParams['paymentmethod']);
+				$message = noborder_get_filled_message($gatewayParams['success_massage'], $verify_request_id, $invoice_id);
+				logTransaction($gatewayParams['name'], ["GET" => $_GET, "POST" => $_POST, "result" => $result], 'Success');
+				Capsule::table('tblinvoices')->where('id', $invoice_id)->update(['notes' => $message]);
+				header('Location: ' . $gatewayParams['systemurl'] . '/viewinvoice.php?id=' . $invoice_id);
+			}
+		
+		}
+	}
+}
+
+function noborder_MetaData(){
+    return array(
+        'DisplayName' => 'noBorder Payment Module',
+        'APIVersion' => '1.1',
+    );
+}
+
+function noborder_config(){
+    return [
+        "FriendlyName" => [
+            "Type" => 'System',
+            "Value" => 'noBorder',
+        ],
+        "api_key" => [
+            "FriendlyName" => 'API key',
+            "Type" => 'text',
+			"Value" => '',
+			"Description" => 'To generate an API key, please refer to the following address. <a href="https://noborder.tech/cryptosite" target="_blank">https://noborder.tech/cryptosite</a>'
+        ],
+        "pay_currency" => [
+            "FriendlyName" => 'Selectable crypto currencies',
+            "Type" => 'text',
+			"Value" => '',
+			"Description" => 'By default, customers can pay through all <a href="https://noborder.tech/cryptosite" target="_blank">active currencies</a> in the gate, but if you want to limit the customer to pay through one or more specific crypto currencies, you can declare the name of the crypto currencies through this variable. If you want to declare more than one currency, separate them with a dash ( - ).', 'woo-noborder-gateway'
+        ],
+        "success_massage" => [
+            "FriendlyName" => 'Success message',
+            "Type" => 'textarea',
+            "Value" => 'Your payment has been successfully completed. <br><br> Invoice id : {invoice_id} <br> Track id: {request_id}',
+            "Description" => 'از طریق این فیلد می توانید متن پیامی را که می خواهید بعد از پرداخت موفق به کاربر نمایش داده شودEnter the message you want to display to the customer after a successful payment. You can also choose these placeholders {request_id}, {invoice_id} for showing the invoice id and the tracking id respectively.'
+        ],
+        "failed_massage" => [
+            "FriendlyName" => 'Failure message',
+            "Type" => 'textarea',
+            "Value" => 'Your payment has failed. Please try again or contact the site administrator in case of a problem. <br><br> Invoice id : {invoice_id} <br> Track id: {request_id}',
+            "Description" => 'Enter the message you want to display to the customer after a failure occurred in payment. You can also choose these placeholders {request_id}, {invoice_id} for showing the invoice id and the tracking id respectively.'
+        ]
+    ];
+}
+
+function noborder_link($params){
+	if ($_SESSION['uid'] <= 0)
+		$htmlOutput .= '<a href="/clientarea.php" class="btn btn-success btn-sm" id="btnPayNow" value="Submit"> Login / Register </a>';
+	else {
+		$htmlOutput = '<form method="get" action="modules/gateways/noborder.php">';
+		$htmlOutput .= '<input type="hidden" name="action" value="pay">';
+		$htmlOutput .= '<input type="hidden" name="invoiceid" value="' . $params['invoiceid'] . '">';
+		$htmlOutput .= '<input type="hidden" name="currency" value="' . $params['currency'] . '">';
+		$htmlOutput .= '<button type="submit" class="btn btn-success btn-sm" id="btnPayNow" value="Submit"> Pay now </button>';
+		$htmlOutput .= '</form>';
+	}
+    return $htmlOutput;
+}
+
+function noborder_get_filled_message($massage, $request_id, $invoice_id){
+    return str_replace(["{request_id}", "{invoice_id}"], [$request_id, $invoice_id], $massage);
+}
+
---
+++ readme.md
@@ -0,0 +1,127 @@
+# noBorder Crypto Payment Gateway for WHMCS
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Configuration](#configuration)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Troubleshooting](#troubleshooting)
+- [License](#license)
+- [Contact Information](#contact-information)
+- [Changelog](#changelog)
+
+---
+
+## Description
+
+The noBorder Crypto Payment Gateway for WHMCS is a plugin that allows WHMCS users to accept cryptocurrency payments via the noBorder platform. This plugin is developed by Hanif Zekri Astaneh and is provided by noBorder.tech.
+
+- **Plugin Name:** noBorder Crypto Payment Gateway for WHMCS
+- **Version:** 1.1
+- **Author:** noBorder.tech
+- **Author URI:** [https://noBorder.tech](https://noBorder.tech)
+- **Author Email:** info@noBorder.tech
+- **Text Domain:** noBorder_whmcs_payment_module
+- **WC tested up to:** 8.8
+- **Copyright (C):** 2020 noBorder
+- **License:** [GPLv3 or later](http://www.gnu.org/licenses/gpl-3.0.html)
+
+---
+
+## Installation
+
+After submitting your website on noBorder.tech and getting an API key, to install the noBorder Crypto Payment Gateway for WHMCS, follow these steps:
+
+1. Copy the `noBorder.php` file to the `modules/gateways/` directory of your WHMCS installation.
+
+2. Log in to your WHMCS admin panel.
+
+3. Go to the "Setup" menu and select "Payment Gateways."
+
+4. Under the "All Payment Gateways" tab, you should see "noBorder" in the list of gateways. Click on it to configure the settings.
+
+5. Configure the gateway settings as described in the [Configuration](#configuration) section below.
+
+6. Once configured, save your settings.
+
+---
+
+## Configuration
+
+To configure the noBorder Crypto Payment Gateway for WHMCS, follow these steps:
+
+1. Log in to your WHMCS admin panel.
+
+2. Go to the "Setup" menu and select "Payment Gateways."
+
+3. Under the "All Payment Gateways" tab, select "noBorder" from the list of gateways.
+
+4. Configure the following settings:
+
+   - **API Key:** Enter your noBorder API key. You can obtain this key by visiting [https://noBorder.tech/cryptosite](https://noBorder.tech/cryptosite).
+
+   - **Pay Currency:** Specify the currencies available for payment. You can list multiple currencies separated by a dash (e.g., BTC-ETH-LTC).
+
+   - **Success Message:** Customize the message to display to customers after a successful payment. You can use placeholders like `{invoice_id}` and `{request_id}` to display the invoice and request IDs.
+
+   - **Failed Message:** Customize the message to display to customers after a failed payment. You can use placeholders like `{invoice_id}` and `{request_id}` to display the invoice and request IDs.
+
+5. Save your settings.
+
+---
+
+## Usage
+
+Once the noBorder Crypto Payment Gateway is configured, customers can use it to make payments for their invoices. Here's how it works:
+
+1. Customers can view and select the noBorder payment option when paying their invoices in the WHMCS client area.
+
+2. Upon selecting noBorder as the payment method, customers will be redirected to complete the payment on the noBorder platform.
+
+3. After making the payment on noBorder, customers will be redirected back to WHMCS.
+
+4. WHMCS will update the invoice status based on the payment result. If the payment is successful, the invoice status will change to "Paid."
+
+---
+
+## Contributing
+
+If you want to contribute to this project or report issues, please visit the GitHub repository: [noBorder Crypto Payment Gateway for WHMCS](https://github.com/hanifzekri/noBorder_whmcs_payment_module).
+
+---
+
+## Troubleshooting
+
+If you encounter any issues or have questions about the noBorder Crypto Payment Gateway for WooCommerce, please refer to the [official documentation](https://noBorder.tech) or contact our support team at [info@noBorder.tech](mailto:info@noBorder.tech).
+
+---
+
+## License
+
+This project is licensed under the GPLv3 or later. See the [GNU General Public License](http://www.gnu.org/licenses/gpl-3.0.html) for more details.
+
+---
+
+## Contact Information
+
+If you have any questions or need assistance, please contact us at [info@noBorder.tech](mailto:info@noBorder.tech).
+
+---
+
+## Changelog
+
+- 1.1, feb 18, 2023
+Bugs fixed
+
+- 1.0, jan 15, 2023
+First release.
+
+---
+
+Thank you for using the noBorder Crypto Payment Gateway for WooCommerce! We appreciate your business.
+
+---
+
+*Copyright (C) 2020 noBorder*
